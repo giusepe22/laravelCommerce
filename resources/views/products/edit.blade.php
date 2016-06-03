2952cd151adcea6aed1cc76c64ca7d38bb205251
Giusepe <giusepebatista@hotmail.com>
2016-06-03 18:24:13 -0300

    resovendo crud create

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -37,24 +37,13 @@
                     {!! Form::text('price', $product->price, ['class'=>'form-control']) !!}
                 </div>
 
-                <div class="form-group">
-                    {{--{!! Form::label('featured', 'Featured:') !!}--}}
-                    {{--{!! Form::checkbox('featured', 1, $product->featured, ['class'=>'form-control']) !!}--}}
+            <div class="form-group">
+                {!! Form::label('featured', 'Featured:') !!}
+                {!! Form::checkbox('featured', $product->featured, ['class' => 'form-control']) !!}
 
-                    {!! Form::label('Featured ?') !!}
-                    {!! Form::radio('featured', 1 ,($product->featured)?true:false, ['class' => 'field']) !!} Yes
-                    {!! Form::radio('featured', 0 ,(!$product->featured)?true:false, ['class' => 'field']) !!} No
-                </div>
-            <!--&nbsp;&nbsp;-->
-
-                <div class="form-group">
-                    {{--{!! Form::label('recommended', 'Recommended:') !!}--}}
-                    {{--{!! Form::checkbox('recommended', 1, $product->recommended, ['class'=>'form-control']) !!}--}}
-
-                    {!! Form::label('Recommend ?') !!}
-                    {!! Form::radio('recommend', 1,($product->recommend)?true:false, ['class' => 'field']) !!} Yes
-                    {!! Form::radio('recommend', 0,(!$product->recommend)?true:false, ['class' => 'field']) !!} No
-                </div>
+                {!! Form::label('recommend', 'Recommend:') !!}
+                {!! Form::checkbox('recommend', $product->recommend, ['class' => 'form-control']) !!}
+            </div>
 
 
             <div class="form-group">
