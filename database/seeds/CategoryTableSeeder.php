11bac3e3827e3f50f3027b0d63738b0ff81e48d0
Giusepe <giusepebatista@hotmail.com>
2016-06-04 01:36:22 -0300

    ajeitando o .env.example

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -1,29 +1,26 @@
 <?php
-use App\Category;
+
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
-class CategoryTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('categories')->truncate();
+class CategoryTableSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		DB::table('categories')->truncate();
 
-        factory('App\Category', 10)->create();
+		factory('CodeComerce\Category', 10)->create();
 
-        #$faker = Faker::create();
+		#$faker = Faker::create();
 
-//        foreach (range(1, 15) as $i) {
-//
-//            Category::create([
-//                'name' => $faker->word(),
-//                'description' => $faker->sentence()
-//            ]);
-//        ?}
-    }
+		//        foreach (range(1, 15) as $i) {
+		//
+		//            Category::create([
+		//                'name' => $faker->word(),
+		//                'description' => $faker->sentence()
+		//            ]);
+		//        ?}
+	}
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -1,45 +1,41 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User;
-use Faker\Factory as Faker;
 
-class UserTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('users')->truncate();
+class UserTableSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		DB::table('users')->truncate();
 
-//        $faker = Faker::create();
-//
-//        User::create([
-//            'name' => 'giusepe',
-//            'email' => 'giusepe@r8digital.com.br',
-//            'password' => Hash::make(123),
-//        ]);
-//
-//        foreach (range(1,5) as $i){
-//
-//            User::create([
-//                'name' => $faker->name(),
-//                'email' => $faker->email,
-//                'password' => Hash::make($faker->word)
-//            ]);
-//        }
+		//        $faker = Faker::create();
+		//
+		//        User::create([
+		//            'name' => 'giusepe',
+		//            'email' => 'giusepe@r8digital.com.br',
+		//            'password' => Hash::make(123),
+		//        ]);
+		//
+		//        foreach (range(1,5) as $i){
+		//
+		//            User::create([
+		//                'name' => $faker->name(),
+		//                'email' => $faker->email,
+		//                'password' => Hash::make($faker->word)
+		//            ]);
+		//        }
 
-        factory('App\User')->create([     //cria so o meu user
-            'name'     => 'giusepe',
-            'email'    => 'giusepe@r8digital.com.br',
-            'password' => Hash::make(123)
-        ]);
+		factory('CodeComerce\User')->create([//cria so o meu user
+				'name'     => 'giusepe',
+				'email'    => 'giusepe@r8digital.com.br',
+				'password' => Hash::make(123)
+			]);
 
-        factory('App\User', 10)->create(); # cria varias de uma, no caso 10
+		factory('CodeComerce\User', 10)->create();# cria varias de uma, no caso 10
 
-    }
+	}
 
 }
