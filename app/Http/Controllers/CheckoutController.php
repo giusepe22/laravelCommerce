85efd52d3b23d699f004e758dd370028e9bca566
Giusepe B <giusepebatista@hotmail.com>
2016-07-11 22:05:53 -0300

    commit Order 0.2

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,115 +4,100 @@ namespace CodeCommerce\Http\Controllers;
 
 use CodeCommerce\Order;
 use CodeCommerce\OrderItem;
-use Illuminate\Http\Request;
 
-use CodeCommerce\Http\Requests;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
-class CheckoutController extends Controller
-{
+class CheckoutController extends Controller {
 
-    public function place(Order $orderModel, OrderItem $orderItem){
+	public function place(Order $orderModel, OrderItem $orderItem) {
 
-        if (!Session::has('cart')){
-            return false;
-        }
-        $cart = Session::get('cart');
+		if (!Session::has('cart')) {
+			return false;
+		}
+		$cart = Session::get('cart');
 
-        if ($cart->getTotal() > 0){
+		if ($cart->getTotal() > 0) {
 
-            $order = $orderModel->create(['user_id' => 1, 'total' => $cart->getTotal()]);
+			$order = $orderModel->create(['user_id' => 1, 'total' => $cart->getTotal()]);
 
-            foreach ($cart->all() as $k => $item){
+			foreach ($cart->all() as $k => $item) {
 
-                $order->items()->create(['product_id' =>$k, 'price' =>$item['price'], 'qtd' =>$item['qtd']] );
+				$order->items()->create(['product_id' => $k, 'price' => $item['price'], 'qtd' => $item['qtd']]);
 
-            }
+			}
 
-            dd($order->items());
+			dd($order->items);
 
-        }
-    }
+		}
+	}
 
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index() {
+		//
+	}
 
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create() {
+		//
+	}
 
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request) {
+		//
+	}
 
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id) {
+		//
+	}
 
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id) {
+		//
+	}
 
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id) {
+		//
+	}
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id) {
+		//
+	}
 }
--- database/migrations/2016_07_07_021938_create_orders_table.php
+++ database/migrations/2016_07_07_021938_create_orders_table.php
@@ -1,35 +1,32 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateOrdersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('orders', function (Blueprint $table) {
-            
-            $table->increments('id');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->decimal('total', 8, 2);
-            $table->smallInteger('status')->default(0);
-            $table->timestamps();
-        });
-    }
+class CreateOrdersTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('orders', function (Blueprint $table) {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('orders');
-    }
+				$table->increments('id');
+				$table->integer('user_id')->unsigned();
+				$table->foreign('user_id')->references('id')->on('users');
+				$table->decimal('total', 8, 2);
+				$table->smallInteger('status')->default(0);
+				$table->timestamps();
+			});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('orders');
+	}
 }
